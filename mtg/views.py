8012a59f627501bac2ddd17de0a415e4b5a5954c
Jason R <sysopfb@gmail.com>
2013-06-06 22:00:25 -0500

    Adding jQuery for searching database

--- mtg/views.py
+++ mtg/views.py
@@ -1,21 +1,33 @@
-# You will probably need more methods from flask but this one is a good start.
-from flask import render_template
+# Import things from flask.
+from flask import render_template, request
+
+# Import func for case insensitive queries.
+from sqlalchemy import func
 
 # Import things from Flask that we need.
 from mtg import app, db
 
-# Import our models
-from models import Card, Edition
+# Import needed database model(s)
+from models import Card
+
 
 # Routing for the server.
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def index():
-    # An example of accessing a card by name and returning the first result.
-    #card = db.session.query(Card).filter(Card.name == 'Llanowar Elves').first()
-    card = db.session.query(Card).filter(Card.name == 'Naturalize').first()
-    
-    # You will need to serve something up here.
-    return render_template('index.html',
-            title = 'Home',
-            card = card,
-            abilities = eval(card.abilities))
+    card = None
+    #Handle POST requests by getting appropriate date
+    if request.method == 'POST':
+        cardname = request.form['card']
+        card = db.session.query(Card).filter(func.lower(Card.name) == func.lower(cardname)).first()
+        #if card is Nonetype then we shouldn't call eval on its member field
+        if card:
+            abilities = eval(card.abilities)
+        else:
+            abilities = None
+        return render_template('index.html',
+                               title='Home',
+                               card=card,
+                               abilities=abilities)
+    else:
+        return render_template('index.html',
+                               title='Home')
