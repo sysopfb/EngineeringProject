6b6087d40bb2c6c83d3f9651e37d79ae3d4ae560
Jason R <sysopfb@gmail.com>
2013-06-12 10:17:37 -0500

    Coded in images
    Now the {} data from the database will be converted to matching images
    by our script when it creates the DOM.

--- mtg/static/card.js
+++ mtg/static/card.js
@@ -10,12 +10,18 @@ $(function() {
                 notfound.hide();
                 var cardTemplate = _.template($('#card-template').html());
                 if(data.found) {
-                    var abils = $('#abilities');
+                    //Defaults so we don't have any null values show up
                     _.defaults(data, {sub_types : "", power : "", toughness : "", cost : "", abilities : "", flavor_text : ""});
+                    //This changes the cost values into {} style
+                    // for ease of conversion later
+                    var newCOST = data.cost.replace(/(.+?)/g, '{$1}');
                     $('#card').html(cardTemplate({url: data.image_url, name: data.name, type: data.type,
                                                   sub_type: data.sub_types, power: data.power, toughness: data.toughness,
-                                                  cost: data.cost, abilities: data.abilities, flavor_text: data.flavor_text,
+                                                  cost: newCOST, abilities: data.abilities, flavor_text: data.flavor_text,
                                                   illustrator: data.illustrator}));
+                    //Easy way to change {} values to images
+                    var newHTML = $('#card').html().replace(/\{(.+?)\}/g, '<img src=static/images/$1.png>');
+                    $('#card').html(newHTML);
                     cardblock.show();
                 } else {
                     notfound.show();
